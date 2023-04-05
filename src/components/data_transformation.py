ab77e4c96fbab8f439fab3259a7d432aff3bc743
abhinavdwivedilko16 <abhinavdwivedilko16@gmail.com>
2023-04-06 00:03:59 +0530

    preprocessor.pkl

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -135,8 +135,7 @@ class DataTransformation:
             input_feature_train_arr=preprocessing_obj.fit_transform(input_feature_train_df)
             input_feature_test_arr=preprocessing_obj.transform(input_feature_test_df)
 
-            train_arr = np.c_[input_feature_train_arr, np.array(target_feature_train_df)]
-            test_arr = np.c_[input_feature_test_arr, np.array(target_feature_test_df)]
+
 
             logging.info(f"Saved preprocessing object.")
 
@@ -148,8 +147,7 @@ class DataTransformation:
             )
 
             return (
-                train_arr,
-                test_arr,
+
                 self.data_transformation_config.preprocessor_obj_file_path,
             )
         except Exception as e:
